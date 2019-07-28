aab8e41b6f9a165e81897481acd5e289f8e3d1b9
Alex kagai <alexkagai@outlook.com>
2019-07-28 20:29:38 +0300

    Defines the ISS query functions

--- iss.py
+++ iss.py
@@ -1,40 +1,65 @@
 """Defines operations with the ISS API"""
+import requests
+import csv
 
-def iss_now():
+def iss_current_loc():
     """ Get ISS Current location data
     """
+    url = "http://api.open-notify.org/iss-now.json"
+    response = requests.get(url)
+    return response.json()
 
-    pass
 
-
-def iss_when_at(lat, long):
+def iss_passing_time(lat, lon):
     """Show when ISS with be at the specified coordinates
     
     Arguments:
     lat -- The latitude value of a location
-    long -- The longitude value of a location
+    lon -- The longitude value of a location
     """
-    
-    pass
+    url = "http://api.open-notify.org/iss-pass.json"
+    coordinates = {
+        "lat":lat,
+        "lon":lon
+    }
+
+    response = requests.get(url, params=coordinates)
+    return response.json()
 
 
-def known_locations():
-    """Show a list of known location and coordinates
+def known_cities():
+    """Show a list of known cities and coordinates
     
     returns dictionary with cities as keys 
     and list of it coordinates
     """
-    pass
+    with open('cities.txt', mode='r') as cities_file:
+        if cities_file is None:
+            print("file does not exist")
+        csv_reader = csv.reader(cities_file, delimiter=',')
+        locations = 0
+        for row in csv_reader:
+            if locations == 0:
+                print(row)
+                locations += 1
+            else:
+                print(", ".join(row))
+                locations+= 1
+
+        if locations <=1:
+            print("Add locations")
+        print(f'\n{locations} Known locations exist')
 
 
-def add_location(city, lat, long):
-    """Add a new location to list of known locations
+def add_city(city, lat, lon):
+    """Add a new city to list of known cities
 
     Arguments:
     city -- name of the location
     lat -- latitude value of a city
-    long -- longitude value of a city
-    
+    lon -- longitude value of a city
     """
-    pass
+    with open('cities.txt', mode='a') as cities_file:
+        city_writer = csv.writer(cities_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        city_writer.writerow([city, lat, lon])
 
